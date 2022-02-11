3c47a4a9dba89c6a21b3235b90a41a080ad85a45
Timothy Smith <timothytsmith8@gmail.com>
2022-02-11 03:58:03 -0800

    Connected to Indexeddb but still need to link up my array to go in the database.

--- list.js
+++ list.js
@@ -1,12 +1,47 @@
 // Lets see what I can do:
 var listItem = document.getElementById("newListItem");
+const dbName = "ListArray";
+var request = window.indexedDB.open(dbName);
+// console.log(request);
+var db;
 // var btn = document.getElementById("btn");
 // var deleteBtn = document.getElementById("deleteBtn");
+request.onerror = function (event) {
+  console.log("not good");
+};
 
 var outputArray = [];
+var customerData = outputArray;
+const listArray = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+];
 
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+
+  const listArray = [
+    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+  ];
+  var objectStore = db.createObjectStore("listArray", {
+    keyPath: `ssn`,
+  });
+
+  // var transaction = event.target.transaction;
+  // console.log(listArray);
+  objectStore.transaction.oncomplete = function (event) {
+    console.log(event);
+    var listItemObjectStore = db
+      .transaction("listArray", "readwrite")
+      .objectStore("listArray");
+    listArray.forEach(function (listItem) {
+      listItemObjectStore.add(listItem);
+    });
+    var transaction = db.transaction(["listArray"], "readwrite");
+  };
+};
 addBtn.addEventListener("click", addElement);
-deleteLastBtn.addEventListener("click", deleteLast);
 function loopi() {
   for (i = 0; i < outputArray.length; i++) {
     var p = document.createElement("p");
@@ -18,7 +53,6 @@ function loopi() {
     p.appendChild(outputElement);
     p.setAttribute("class", "item");
     deleteBtn.setAttribute("id", `delete${i}`);
-    deleteBtn.setAttribute("class", i);
     deleteBtn.addEventListener("click", deleteCurrent);
     outputList.appendChild(p);
   }
@@ -31,21 +65,11 @@ function addElement() {
   loopi();
 }
 
-function deleteLast() {
-  outputArray.pop(listItem.value);
-  outputList.innerHTML = outputArray;
-}
-
 function deleteCurrent() {
   // deleteBtn.className var p = document.createElement("p");
 
   outputList.innerHTML = "";
-  var who = this.id;
-
-  var yes = who.slice(6, 7);
-  console.log(yes);
-  // var who = document.getElementsByClassName(`delete ${i}`);
-  outputArray.splice(yes, 1);
-  console.log(outputArray);
+  var who = this.id.slice(6, 7);
+  outputArray.splice(who, 1);
   loopi();
 }
