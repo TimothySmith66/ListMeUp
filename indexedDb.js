2ead245a1fccf44a4f7febb9e8396f0f3ce8a0ff
Timothy Smith <timothytsmith8@gmail.com>
2022-02-27 15:22:49 -0800

    creating javascript html for the whole project. Regular HTML is just not good enough.

---
+++ indexedDb.js
@@ -0,0 +1,72 @@
+const dbName = "ListArray";
+var request = window.indexedDB.open(dbName);
+// console.log(request);
+var db;
+// var btn = document.getElementById("btn");
+
+request.onerror = function (event) {
+  console.log("not good");
+};
+
+// var customerData = customerData.assign({}, outputArray);
+const listArray = customerData;
+console.log(customerData);
+var customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+];
+
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+  const listArray = customerData;
+  // = [
+  //   { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  //   { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+  // ];
+  var objectStore = db.createObjectStore("listArray", {
+    keyPath: `ssn`,
+  });
+
+  // var transaction = event.target.transaction;
+  // console.log(listArray);
+  objectStore.transaction.oncomplete = function (event) {
+    console.log(event);
+    var listItemObjectStore = db
+      .transaction("listArray", "readwrite")
+      .objectStore("listArray");
+    listArray.forEach(function (listItem) {
+      listItemObjectStore.add(listItem);
+    });
+  };
+};
+// }
+function update() {
+  request.onupgradeneeded = function (event) {
+    db = event.target.result;
+
+    console.log("oh no");
+
+    var transaction = db.transaction(["listArray"], "readwrite");
+    var request = objectStore.get("444-44-4444");
+    request.onerror = (event) => {
+      // Handle errors!
+      console.log("oh no");
+    };
+    request.onsuccess = (event) => {
+      // Get the old value that we want to update
+      var data = event.target.result;
+
+      // update the value(s) in the object that you want to change
+      data.age = 42;
+      console.log("hooray");
+      // Put this updated object back into the database.
+      var requestUpdate = objectStore.put(data);
+      requestUpdate.onerror = (event) => {
+        // Do something with the error
+      };
+      requestUpdate.onsuccess = (event) => {
+        // Success - the data is updated!
+      };
+    };
+  };
+}
--- list.js
+++ list.js
@@ -1,79 +1,31 @@
-// Lets see what I can do:
-var listItem = document.getElementById("newListItem");
-const dbName = "ListArray";
+// Lets see what I can do:x
+var gridDiv = document.createElement("div");
+
+gridDiv.setAttribute("class", "grid");
+document.body.appendChild(gridDiv);
+/* <input type="text" id="newListItem" name="newListItem"></input>; */
+var listItem = document.createElement("INPUT");
+listItem.setAttribute("type", "text");
+listItem.setAttribute("id", "newListItem");
+gridDiv.appendChild(listItem);
+
+/* <button class="btn" id="addBtn">submit</button> */
+
+var AddButton = document.createElement("button");
+
+AddButton.setAttribute("id", "btn");
+AddButton.innerHTML = "submit";
+gridDiv.appendChild(AddButton);
+
+/* <output output ></output>; */
+var outputList = document.createElement("OUTPUT");
+// listItem.setAttribute("type", "text");
+listItem.setAttribute("id", "outputList");
+gridDiv.appendChild(outputList);
+
 var outputArray = [];
 // function firstTry() {
-var request = window.indexedDB.open(dbName);
-// console.log(request);
-var db;
-// var btn = document.getElementById("btn");
 
-request.onerror = function (event) {
-  console.log("not good");
-};
-
-// var customerData = customerData.assign({}, outputArray);
-const listArray = customerData;
-console.log(customerData);
-var customerData = [
-  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
-];
-
-request.onupgradeneeded = function (event) {
-  db = event.target.result;
-  const listArray = customerData;
-  // = [
-  //   { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  //   { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
-  // ];
-  var objectStore = db.createObjectStore("listArray", {
-    keyPath: `ssn`,
-  });
-
-  // var transaction = event.target.transaction;
-  // console.log(listArray);
-  objectStore.transaction.oncomplete = function (event) {
-    console.log(event);
-    var listItemObjectStore = db
-      .transaction("listArray", "readwrite")
-      .objectStore("listArray");
-    listArray.forEach(function (listItem) {
-      listItemObjectStore.add(listItem);
-    });
-  };
-};
-// }
-function update() {
-  request.onupgradeneeded = function (event) {
-    db = event.target.result;
-
-    console.log("oh no");
-
-    var transaction = db.transaction(["listArray"], "readwrite");
-    var request = objectStore.get("444-44-4444");
-    request.onerror = (event) => {
-      // Handle errors!
-      console.log("oh no");
-    };
-    request.onsuccess = (event) => {
-      // Get the old value that we want to update
-      var data = event.target.result;
-
-      // update the value(s) in the object that you want to change
-      data.age = 42;
-      console.log("hooray");
-      // Put this updated object back into the database.
-      var requestUpdate = objectStore.put(data);
-      requestUpdate.onerror = (event) => {
-        // Do something with the error
-      };
-      requestUpdate.onsuccess = (event) => {
-        // Success - the data is updated!
-      };
-    };
-  };
-}
 // when enter key is pressed in the text area
 listItem.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -81,12 +33,15 @@ listItem.addEventListener("keyup", function (event) {
   }
 });
 // when the button is pressed
-addBtn.addEventListener("click", addElement);
+AddButton.addEventListener("click", addElement);
 // appends all array items to page
 function loopi() {
   for (i = 0; i < outputArray.length; i++) {
     var p = document.createElement("p");
     const outputElement = document.createElement("button");
+    outputElement.setAttribute("id", "ListItem");
+
+    p.setAttribute("class", "item");
     outputElement.innerHTML = outputArray[i];
     var deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = "x";
